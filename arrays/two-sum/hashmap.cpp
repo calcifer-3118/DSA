598541e5c77a2fe6fed3883eb8f92757d324cda9
calcifer <dheerjain5002000@gmail.com>
2026-04-06 06:45:33 +0530

    two-sum

---
+++ arrays/two-sum/hashmap.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+         unordered_map<int, int> hm;
+
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(hm.count( target - nums[i] ))
+                return {i, hm[target - nums[i]]};
+            hm[nums[i]] = i;
+        }
+        
+        return {};
+
+    }
+};
---
+++ arrays/two-sum/readme.md
@@ -0,0 +1,17 @@
+# Two Sum
+
+## Approaches
+
+### 1. Brute Force
+- Compare every pair
+- Time: O(n²)
+- Space: O(1)
+
+### 2. HashMap
+- Store elements in map
+- Time: O(n)
+- Space: O(n)
+
+## Key Learning
+- Tradeoff between time and space
+- Hashing reduces time complexity significantly
