4adb3928e396fe1c32a0fdcd056a581947fe13c9
calcifer <dheerjain5002000@gmail.com>
2026-04-17 01:13:35 +0530

    palindrome-number

--- two-pointers/valid-palindrome/twoPointer.cpp
+++ two-pointers/valid-palindrome/twoPointer.cpp
@@ -1,22 +1,29 @@
 class Solution {
     public:
         bool isPalindrome(string s) {
+            int left=0, right=s.length()-1;
     
-           int left = 0, right = s.length() - 1;
-    
-            while (left < right) {
-                if (!isalnum(s[left]))
+            while(left < right)
+            {
+                if(!isAlphaNum(s[left]))
                     left++;
-                else if (!isalnum(s[right]))
+                else if(!isAlphaNum(s[right]))
                     right--;
-                else {
-                    if (tolower(s[left]) != tolower(s[right]))
+                else{
+                    if(tolower(s[left]) != tolower(s[right]))
                         return false;
                     left++;
                     right--;
-                }
+                } 
             }
+    
             return true;
         }
-    };
     
+        bool isAlphaNum(char c)
+        {
+            return ( c >= 'A' && c <= 'Z' ||
+                     c >= 'a' && c <= 'z' ||
+                     c >= '0' && c <= '9');
+        }
+};
